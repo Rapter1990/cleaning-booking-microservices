19241cd1e9e9516796903de2f8edb5f32226e4bf
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2026-03-10 17:54:25 +0300

    Task 23 : Add Pagination Process in Common Module

---
+++ common/src/main/java/com/booking/common/model/dto/request/CustomPagingRequest.java
@@ -0,0 +1,52 @@
+package com.booking.common.model.dto.request;
+
+import com.booking.common.model.pagination.CustomPaging;
+import com.booking.common.model.pagination.CustomSorting;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.io.Serializable;
+
+/**
+ * Abstract class that represents a custom paging request.
+ * This class provides pagination details through a `CustomPaging` object
+ * and includes a method to convert the pagination information into a `Pageable` object.
+ * It is intended to be extended by other classes that require paging functionality.
+ */
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class CustomPagingRequest implements Serializable {
+
+    @NotNull
+    @Valid
+    private CustomPaging pagination;
+
+    private CustomSorting sorting; // Optional
+
+    /**
+     * Converts the request into a Spring Data {@link Pageable} object
+     * with optional sorting.
+     *
+     * @return Pageable instance based on pagination and sorting settings
+     */
+    public Pageable toPageable() {
+        Sort sort = (sorting != null) ? sorting.toSort() : Sort.unsorted();
+
+        return PageRequest.of(
+                Math.toIntExact(pagination.getPageNumber()),
+                Math.toIntExact(pagination.getPageSize()),
+                sort
+        );
+
+    }
+
+}
---
+++ common/src/main/java/com/booking/common/model/pagination/CustomPaging.java
@@ -0,0 +1,35 @@
+package com.booking.common.model.pagination;
+
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+/**
+ * Represents paging parameters for paginated API requests.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class CustomPaging implements Serializable {
+
+    @Min(value = 1, message = "Page number must be bigger than 0")
+    private Integer pageNumber;
+
+    @Min(value = 1, message = "Page size must be bigger than 0")
+    private Integer pageSize;
+
+    /**
+     * Returns a zero-based page index compatible with Spring Data.
+     *
+     * @return the page index (zero-based)
+     */
+    public Integer getPageNumber() {
+        return pageNumber - 1;
+    }
+
+}
